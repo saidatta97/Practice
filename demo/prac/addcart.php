7222336dd9f5605c4b2547a59c8498b77dd037b8
Saidatta Sawant <32416803+saidatta97@users.noreply.github.com>
2020-12-31 19:55:19 +0530

    duplicates files
    duplicates

--- demo/prac/addcart.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("config.php");
-$id=$_GET['id'];
-
-mySQLi_query($mysqli,"INSERT INTO cart (poduct_id,quantity)
-			VALUES ('$id',1)");
-echo "<script>alert('Added to cat'); window.location='index.php'</script>"
-
-?>
